d4b938fda0f2f4a4eb2f7dd7d8409b920507e950
Gustavo <garv1998131@gmail.com>
2021-04-28 22:28:14 -0300

    initial commit

---
+++ project-zero-server/structure.ts
@@ -0,0 +1,16 @@
+interface User {
+	id: string;
+	data: {
+		age: number;
+		direction: string;
+		country: string;
+		firstName: string;
+		lastName: string;
+		fullName: string;
+	};
+	status: {
+		disabled: boolean;
+		active: boolean;
+		lastConnection: Date;
+	};
+}
