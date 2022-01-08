c7b2128a9facb193c506699734bbf344f1a53a6b
gustavo rojas <garv1998131@gmail.com>
2022-01-08 17:46:03 -0300

    added script, cluster

--- project-zero-server/src/app.ts
+++ project-zero-server/src/app.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
 import graphqlConfig from "./graphql";
+import os from "os";
+import cluster from "cluster";
 import "colors";
 
 const app = express();
 
 const port = process.env.PORT || 8080;
+const cpuNum = os.cpus().length;
 
 app.use(
 	"/graphql",
@@ -15,4 +18,14 @@ app.use(
 	})
 );
 
-app.listen(port, () => console.log("Running GraphQL API server in port:".green, port));
+if (cluster.isMaster) {
+	for (let i = 0; i < cpuNum; i++) {
+		cluster.fork();
+	}
+	cluster.on("exit", (worker, code, signal) => {
+		console.log(`Worker ${worker.process.pid} died`);
+		cluster.fork();
+	});
+} else {
+	app.listen(port, () => console.log(`Running GraphQL API server in port: ${port} - pid: ${process.pid}`.green));
+}
